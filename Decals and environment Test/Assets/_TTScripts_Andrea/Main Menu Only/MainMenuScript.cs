9425c8d42ec2e6568166b9765ebbdca90c3f8293
andrea-caramia <twentysevenderp@gmail.com>
2019-12-08 16:56:11 +0000

    Updated main menu scene (added button behaviour, navigation and transition)

---
+++ Decals and environment Test/Assets/_TTScripts_Andrea/Main Menu Only/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [SerializeField] Image dissolvePanel;
+    [SerializeField] AudioSource audio;
+    public void LoadWorld(int level)
+    {
+        StartCoroutine(Dissolve(level));
+    }
+
+    private IEnumerator Dissolve(int level)
+    {
+        Color startColor = dissolvePanel.color;
+        float startVolume = audio.volume;
+        float lapsed = 0f;
+        
+        while (lapsed < 1.0f)
+        {
+            lapsed += Time.deltaTime / 2;
+            dissolvePanel.color = Color.Lerp(startColor, new Color(0, 0, 0, 1), lapsed);
+            audio.volume = Mathf.Lerp(startVolume, 0, lapsed + 0.15f);
+            yield return null;
+        }
+
+        SceneManager.LoadSceneAsync(level);
+    }
+}
