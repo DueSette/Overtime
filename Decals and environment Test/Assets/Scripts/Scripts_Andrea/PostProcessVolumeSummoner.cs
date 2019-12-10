11fce466525c938da2cc75d4d6f0a07058dd7191
andrea-caramia <twentysevenderp@gmail.com>
2019-12-10 18:21:04 +0000

    Fixed inventory (something broke it, just in case next time tell me before touching UI), remade post proc for wake up scene and transition, reintegrated timeline director and asset,  WIP runtime post proc script

---
+++ Decals and environment Test/Assets/Scripts/Scripts_Andrea/PostProcessVolumeSummoner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessVolumeSummoner : MonoBehaviour
+{
+    /*
+     * 
+     * REQUIREMENTS
+     * Choose a Volume profile from list
+     * Equip it to deisgnated GameObject
+     * Choose speed, proximity, lingering (how long to stay at a certain proximity)
+     */
+}
