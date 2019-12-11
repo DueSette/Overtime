3cc94ba1812dabb0f5e5803368db649d78c7a5b7
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2019-12-11 10:44:00 +0000

    Level 1 - Player Spawning

---
+++ Decals and environment Test/Assets/Scripts/Level Generator/Level Generator/LayoutData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayoutData : MonoBehaviour
+{
+    public List<OfficeRoomController> layoutRooms = new List<OfficeRoomController>();
+}
