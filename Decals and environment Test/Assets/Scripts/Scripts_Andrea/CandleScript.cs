3c9a57f51353ca614fbde985cd2c4009c93621e5
andrea-caramia <twentysevenderp@gmail.com>
2019-12-16 01:21:54 +0000

    Kid's shadow, part of level 0, candle behaviours added

---
+++ Decals and environment Test/Assets/Scripts/Scripts_Andrea/CandleScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleScript : MonoBehaviour
+{
+    Rigidbody rb;
+    AudioSource aud;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        aud.Play();
+        transform.parent = null;
+        rb.isKinematic = false;
+        GetComponent<Collider>().isTrigger = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        aud.Play();
+    }
+}
