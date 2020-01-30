26c6edeac4d2ac2829cea70ed28f2ab49f5b7702
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2020-01-30 12:15:55 +0000

    Revert "Gutting Pointless Assets"
    This reverts commit 62f116b2b164b1f58a8ec210051543b9c33dfbad.

---
+++ Decals and environment Test/Assets/Scripts/FlickerLight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour
+{
+    Light testLight;
+    public float minWaitTime;
+    public float maxWaitTime;
+    public GameObject slender;
+    public AudioSource audio;
+    public AudioClip clip;
+
+    void Start()
+    {
+        testLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            testLight.enabled = !testLight.enabled;
+            yield return new WaitForSeconds(minWaitTime);
+            testLight.enabled = !testLight.enabled;
+
+        }
+    }
+
+    private void Update()
+    {
+        if (testLight.isActiveAndEnabled)
+        {
+            slender.SetActive(false);
+        }
+        else
+        {
+            slender.SetActive(true);
+            audio.Play();
+        }
+    }
+}
