8128e30116416a866fbd6873676269d3924fd2a7
andrea-caramia <twentysevenderp@gmail.com>
2020-02-16 03:32:18 +0000

    Note scatterer added. Lots of meta files were changed upon editor launch
    We have a dedicated extension class named Extensions

---
+++ Decals and environment Test/Assets/Scripts/Scripts_Andrea/Inventory/Scatterer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    //=====PURPOSE OF THE CLASS IS TO PROVIDE FUNCTIONALITIES FOR PROCEDURALLY SCATTERING NOTES THROUGHOUT LEVEL
+
+public class Scatterer : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> objectsToScatter = new List<GameObject>();
+    private List<ScatterReceiver> receivers = new List<ScatterReceiver>();
+
+    //cycles through the list of scatterables and receivers, if item and slot have the same importance slot the item is assigned to the slot
+    public void ScatterObjects()
+    {
+        foreach(GameObject obj in objectsToScatter)
+        {
+            InGameObjectBaseClass g = obj.GetComponent<InGameObjectBaseClass>();
+            Scatter(g);
+        }
+    }
+
+    void Scatter(InGameObjectBaseClass scatterableItem) //add scatterReceiver
+    {
+        foreach (ScatterReceiver receiver in receivers)
+        {
+            if (receiver.importance == scatterableItem.importance) //if the importance is the same, place the object in the receiver's slot
+            {
+                PlaceItem(scatterableItem.gameObject, receiver.scatterSlot);
+                receivers.Remove(receiver);
+                break;
+            }
+        }
+    }
+
+    void PlaceItem(GameObject item, Transform slot)
+    {
+        GameObject spawnedObj = Instantiate(item, slot);
+        spawnedObj.transform.localPosition = Vector3.zero;
+    }
+
+    #region SetUp Functions
+    private void OnEnable()
+    {
+        ScatterReceiver.RaiseEvent += AddReceiver;
+    }
+
+    private void Start()
+    {
+        receivers.ShuffleList();
+        objectsToScatter.ShuffleList();
+        ScatterObjects();
+    }
+
+    private void OnDisable()
+    {
+        ScatterReceiver.RaiseEvent -= AddReceiver;
+    }
+
+    private void AddReceiver(ScatterReceiver s)
+    {
+        receivers.Add(s);
+    }
+    #endregion
+}
