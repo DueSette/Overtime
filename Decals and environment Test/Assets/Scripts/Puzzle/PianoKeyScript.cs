f47d6bcc334e1761b57ec2e0387c5f9eae2491e3
NinjaBanter <Markfeeney91@hotmail.com>
2020-02-17 03:58:01 +0000

    MusicPzl

---
+++ Decals and environment Test/Assets/Scripts/Puzzle/PianoKeyScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoKeyScript : MonoBehaviour, IInteractable
+{
+
+    [SerializeField] private bool noteActive;
+    [SerializeField] private AudioClip fAud, gAud, eAud, aAud;
+    [SerializeField] private int noteNumber;
+    [SerializeField] AudioSource aud;
+    [SerializeField] private PianoNoteScript pnScript;
+    public GameObject thePiano;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    void IInteractable.InteractWith()
+    {
+        if (this.gameObject.tag == "FNote")
+        {
+            aud.PlayOneShot(fAud);
+            noteNumber = 1;
+   //         Debug.Log("F Pressed");
+            UpdateList();
+        }
+        else
+         if (this.gameObject.tag == "GNote")
+        {
+            aud.PlayOneShot(gAud);
+            noteNumber = 2;
+     //       Debug.Log("G Pressed");
+            UpdateList();
+        }
+        else
+         if (this.gameObject.tag == "ENote")
+        {
+            aud.PlayOneShot(eAud);
+            noteNumber = 3;
+     //       Debug.Log("E Pressed");
+            UpdateList();
+        }
+        else
+         if (this.gameObject.tag == "ANote")
+        {
+            aud.PlayOneShot(aAud);
+            noteNumber = 4;
+      //      Debug.Log("A Pressed");
+            UpdateList();
+        }
+    }
+
+    void UpdateList()
+    {
+        pnScript = thePiano.GetComponent<PianoNoteScript>();
+        pnScript.musicNotes.Add(noteNumber);
+        pnScript.active = true;
+    }
+}
---
+++ Decals and environment Test/Assets/Scripts/Puzzle/PianoNoteScript.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoNoteScript : MonoBehaviour
+{
+
+    public List<int> musicNotes;
+    public bool active = false;
+    public int currentNote;
+    public GameObject thePiano;
+    public AudioSource aud;
+    public AudioClip drawerSound;
+    public Animation keyAnimation;
+    private int listLength;
+    public int desiredNoteOne = 4, desiredNoteTwo = 1, 
+               desiredNoteThree = 2, desiredNote4 = 3;
+
+    // 4 1 2 3
+    // A, F, G, E 
+
+    private void Start()
+    {
+        thePiano = this.gameObject;
+        aud = GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+        listLength = musicNotes.Count;
+
+        if (active)
+        {
+            active = false;
+
+
+            if (listLength >= 1)
+            {
+                currentNote = musicNotes[listLength - 1];
+            }
+
+            if (listLength == 1)
+            {
+                if (currentNote != 4)
+                {
+                    musicNotes.Clear();
+                    Debug.Log(" list cleared ");
+                }
+            }
+
+            if (listLength == 2)
+            {
+                if (currentNote != 1)
+                {
+                    musicNotes.Clear();
+                    Debug.Log(" list cleared ");
+                }
+            }
+
+            if (listLength == 3)
+            {
+                if (currentNote != 2)
+                {
+                    musicNotes.Clear();
+                    Debug.Log(" list cleared ");
+                }
+            }
+
+            if (listLength == 4)
+            {
+                if (currentNote != 3)
+                {
+                    musicNotes.Clear();
+                    Debug.Log(" list cleared ");
+                }
+                else
+                {
+                    Debug.Log(" Hi there ");
+                    thePiano.GetComponent<Animation>().Play("BlockMove");
+                    aud.PlayOneShot(drawerSound);
+                }
+            }
+
+            if (listLength > 4)
+            {
+                musicNotes.Clear();
+                Debug.Log("list limit reached");
+            }
+
+            active = false;
+        }
+
+    }
+
+    void UpdateCurrentList()
+    {
+        if (musicNotes.Count == 0)
+        {
+           // Debug.Log("zero in list");
+        }
+        else
+        {
+          //  Debug.Log("" + musicNotes.Count + "is the adjusted amount in list");
+        }
+    }
+}
