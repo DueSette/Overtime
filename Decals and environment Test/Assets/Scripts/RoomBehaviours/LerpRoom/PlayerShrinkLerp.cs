43fe7fcb523b6eb7aeab07f05985a68618ec3b25
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2020-03-02 15:25:18 +0000

    Lerp Room Behaviour

---
+++ Decals and environment Test/Assets/Scripts/RoomBehaviours/LerpRoom/PlayerShrinkLerp.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerShrinkLerp : MonoBehaviour
+{
+    private bool canLerp;
+    private GameObject playerCharacter;
+    private float furthestPoint = 0;
+
+    [SerializeField] private float sizeMax;
+    [SerializeField] private float sizeMin;
+
+    [SerializeField] private CinemachinePathBase lerpRoomPath;
+
+    /*
+    ====================================================================================================
+    Event Handling
+    ====================================================================================================
+    */
+    private void OnEnable()
+    {
+        LevelManager.onLevelEvent += HandleEvent;
+    }
+    private void OnDisable()
+    {
+        LevelManager.onLevelEvent -= HandleEvent;
+    }
+    void HandleEvent(string eventName)
+    {
+        if (eventName == "LerpRoom")
+        {
+            canLerp = true;
+            playerCharacter = GameStateManager.GetPlayer();
+        }
+    }
+
+
+    /*
+    ====================================================================================================
+    Lerping Player
+    ====================================================================================================
+    */
+    // Update is called once per frame
+    void Update()
+    {
+        if (canLerp)
+        {
+            LerpPlayer();
+        }
+    }
+
+    private void LerpPlayer()
+    {
+        // Getting the closest point on the cinemachine path
+        float newPoint = lerpRoomPath.FindClosestPoint(playerCharacter.transform.position, 0, -1, 10);
+
+        // ensuring that the player doesn't grow in size
+        if (newPoint > furthestPoint)
+        {
+            furthestPoint = newPoint;
+        }
+
+        // calculating new player height
+        float maxPoint = lerpRoomPath.MaxUnit(CinemachinePathBase.PositionUnits.PathUnits);
+        float lerpPoint = furthestPoint / maxPoint;
+
+        float newScale = Mathf.Lerp(sizeMax, sizeMin, lerpPoint);
+        playerCharacter.transform.localScale = new Vector3(newScale, newScale, newScale);
+    }
+}
