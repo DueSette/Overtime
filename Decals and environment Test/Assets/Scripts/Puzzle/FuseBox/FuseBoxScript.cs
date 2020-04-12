e676868101c444fe08f9f132f2825f5de32b3497
DueSette <44945710+DueSette@users.noreply.github.com>
2020-04-12 19:34:03 +0100

    Added last features to the bloody fusebox puzzle, everything seems to be in

--- Decals and environment Test/Assets/Scripts/Puzzle/FuseBox/FuseBoxScript.cs
+++ Decals and environment Test/Assets/Scripts/Puzzle/FuseBox/FuseBoxScript.cs
@@ -13,16 +13,21 @@ public class FuseBoxScript : MonoBehaviour, IInteractable
     [SerializeField, Tooltip("Each fuse object that STARTS within the box - do not put inventory fuses here")]
     List<GameObject> fusePrefabs = new List<GameObject>();
 
-    [SerializeField] AudioClip idleSolved, doorOpen, doorClose, pickFuse, putFuseTray, solvedSound, unsolvedSound;
+    [SerializeField] AudioClip idleSolvedSound, doorOpen, doorClose, pickFuse, putFuseTray, solvedSound, unsolvedSound;
 
     FuseSlotScript[] fuseSlots; //all the places where you can fit a fuse in, they are classes as they contain a package of info
-
+    
     FuseTrayScript tray;
     Animator anim;
-    [SerializeField] AudioSource aud, idleAud;
+    [SerializeField] AudioSource aud;
 
     [SerializeField] LayerMask fuseBoxLayer;
     [HideInInspector] public GameObject currentlyHeldFuse = null; //the fuse the player is currently using
+
+    [SerializeField] MeshRenderer mainShell, hubShell, lightningBolt;
+    [SerializeField] List<MeshRenderer> ledIndicators = new List<MeshRenderer>();
+    [SerializeField] Material correctSolvedMaterial;
+    private List<Material> mainShellMaterials = new List<Material>();
 
     enum PuzzleState { ACTIVE = 0, PASSIVE = 2, SOLVED = 4 }
     PuzzleState state = PuzzleState.PASSIVE;
@@ -30,6 +35,7 @@ public class FuseBoxScript : MonoBehaviour, IInteractable
     int slotsFilled = 0; //used for keeping track of how many items have been spawned on the tray, internal logic
     private float cameraDist; //will be used as the distance between the player cam and the held fusebox
 
+    #region Unity methods
     private void Start()
     {
         tray = GetComponentInChildren<FuseTrayScript>();
@@ -46,6 +52,8 @@ public class FuseBoxScript : MonoBehaviour, IInteractable
             fuseSlots[i] = f;
             i++;
         }
+
+        mainShell.GetMaterials(mainShellMaterials);
     }
 
     private void Update()
@@ -56,6 +64,17 @@ public class FuseBoxScript : MonoBehaviour, IInteractable
 
         if (currentlyHeldFuse != null) { UpdateHeldFusePosition(); }
     }
+
+    private void OnEnable()
+    {
+        UnityStandardAssets.Characters.FirstPerson.FirstPersonController.ExitInteraction += ExitInteraction;
+    }
+    private void OnDisable()
+    {
+        UnityStandardAssets.Characters.FirstPerson.FirstPersonController.ExitInteraction -= ExitInteraction;
+    }
+
+    #endregion
 
     #region Interaction
     void CheckFuseboxInteraction()
@@ -109,12 +128,27 @@ public class FuseBoxScript : MonoBehaviour, IInteractable
         tray.enabled = false; //prevents mishaps from happening
     }
 
+    public IEnumerator SetSolvedState()
+    {
+        StartCoroutine(DelaySound(idleSolvedSound));
+
+        state = PuzzleState.SOLVED;
+
+        ManageColouredStrips(true);
+        TurnOnLightsOnSolved();
+
+        yield return new WaitForSeconds(1.75f);
+        aud.PlayOneShot(solvedSound);
+        anim.SetTrigger("Close");
+
+        GameStateManager.SetGameState(GameState.IN_GAME);
+    }
     #endregion
 
     #region Startup Methods
     void IInteractable.InteractWith()
     {
-        if(state == (PuzzleState.ACTIVE & PuzzleState.SOLVED)) { return; }
+        if(state == (PuzzleState.ACTIVE | PuzzleState.SOLVED)) { return; }
 
         state = PuzzleState.ACTIVE;
 
@@ -149,30 +183,39 @@ public class FuseBoxScript : MonoBehaviour, IInteractable
             if (InventoriesManager.instance.HasItemAndRemove(s, out newFuse))
                 fusePrefabs.Add(newFuse);
     }
+    #endregion
 
-    private void OnEnable()
+    #region Lights, strips, and indicator manipulation
+
+    public void ManageColouredStrips(int stripToChange, Color32 col) //set a single strip to a given colour specified in the slotscript
     {
-        UnityStandardAssets.Characters.FirstPerson.FirstPersonController.ExitInteraction += ExitInteraction;
-    }
-    private void OnDisable()
-    {
-        UnityStandardAssets.Characters.FirstPerson.FirstPersonController.ExitInteraction -= ExitInteraction;
-    }
-
-#endregion
-
-    public void SetSolvedState()
-    {
-        aud.PlayOneShot(solvedSound);
-        StartCoroutine(DelaySound(idleSolved));
-
-        state = PuzzleState.SOLVED;
-        anim.SetTrigger("Close");
-
-        GameStateManager.SetGameState(GameState.IN_GAME);
+        mainShellMaterials[stripToChange].SetColor("_EmissiveColor", new Color(col.r, col.g, col.b, 4));
     }
 
-    IEnumerator DelaySound(AudioClip clip)
+    public void ManageColouredStrips(bool correct) //change the colour of every strip at once
+    {
+        for (int i = 1; i < mainShellMaterials.Count; i++)
+        {
+            mainShellMaterials[i].SetColor("_EmissiveColor", correct ? new Color(0, 66, 255) : new Color(255, 0, 0));
+            ledIndicators[i - 1].material.SetColor("_EmissiveColor", correct ? new Color(0, 66, 255) : new Color(255, 0, 0));
+        }
+    }
+
+    public void ResetLedIndicators() //turns off every indicator
+    {
+        for (int i = 0; i < ledIndicators.Count; i++)
+            ledIndicators[i].material.SetColor("_EmissiveColor", Color.black);
+    }
+
+    private void TurnOnLightsOnSolved()
+    {
+        hubShell.material = correctSolvedMaterial;
+        lightningBolt.material.SetColor("_EmissiveColor", new Color(0, 66, 255));
+    }
+
+    #endregion
+
+    IEnumerator DelaySound(AudioClip clip) //simply delays a sound
     {
         float lapsed = 0.0f;
 
