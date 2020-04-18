5e17264511cacc55522d35cfb9cc93e110c4cb8e
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2020-04-18 03:48:52 +0100

    Setup SO In Level 1 Maze

--- Decals and environment Test/Assets/Scripts/Scripts_Andrea/Level 1 Only/SOScript.cs
+++ Decals and environment Test/Assets/Scripts/Scripts_Andrea/Level 1 Only/SOScript.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class SOScript : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    [SerializeField] private TravelDirection directionOfMovement;
+    [SerializeField] private Transform triggerPoint;
+    [SerializeField] private float timeBeforeFade;
+    [SerializeField] private float timeForFade;
+    [SerializeField] private Material fadeMaterial;
+    [SerializeField] private Color defaultColor;
+    [SerializeField] private Color fadedColor;
 
     bool canTrigger = true;
 
@@ -27,7 +34,7 @@ public class SOScript : MonoBehaviour
     bool IsPlayerNear()
     {
         Vector3 playerPos = GameStateManager.GetPlayer().transform.position;
-        if (Vector3.Distance(playerPos, transform.position) < 5)
+        if (Vector3.Distance(playerPos, triggerPoint.transform.position) < 5)
         {
             return true;
         }
@@ -56,97 +63,43 @@ public class SOScript : MonoBehaviour
         // Event Start
         canTrigger = false;
 
-        // Rotating SO
-        // Moving SO
-        yield return null;
+        // Movement Animation
+        switch (directionOfMovement)
+        {
+            case (TravelDirection.LEFT):
+                anim.SetTrigger("TurnLeft");
+                break;
+
+            case (TravelDirection.RIGHT):
+                anim.SetTrigger("TurnRight");
+                break;
+        }
+
+        yield return new WaitForSeconds(timeBeforeFade);
+
+        // Fade Animation
+        float t = 0;
+        while (t < 1.0f)
+        {
+            t += (Time.deltaTime / timeForFade);
+
+            Color newColor = Color.Lerp(defaultColor, fadedColor, t);
+
+            fadeMaterial.SetColor("_TintColor", newColor);
+
+            yield return null;
+        }
 
 
         // SO Is No Longer Needed
-        this.enabled = false;
+        anim.gameObject.SetActive(false);
+        // Resetting Material Visibility
+        fadeMaterial.SetColor("_TintColor", defaultColor);
     }
 
-
-    /*
-    ====================================================================================================
-    Utility
-    ====================================================================================================
-    */
-    private IEnumerator LerpTowardPosition(Vector3 startPos, Vector3 endPos, float time, LerpType lerpType)
+    private enum TravelDirection
     {
-        float t = 0;
-        while (t < 1)
-        {
-            t += (Time.deltaTime / time);
-            Vector3 newPos = Vector3.Lerp(startPos, endPos, CalculateLerpType(t, lerpType));
-            this.transform.position = newPos;
-
-            yield return null;
-        }
-
-        this.transform.position = endPos;
-    }
-
-    private IEnumerator LerpTowardRotation(Vector3 startRot, Vector3 endRot, float time, LerpType lerpType)
-    {
-        float t = 0;
-        while (t <= 1)
-        {
-            t += (Time.deltaTime / time);
-            Vector3 newRot = Vector3.Lerp(startRot, endRot, CalculateLerpType(t, lerpType));
-            this.transform.eulerAngles = newRot;
-
-            yield return null;
-        }
-
-        this.transform.eulerAngles = endRot;
-    }
-
-    private float CalculateLerpType(float lerpTime, LerpType type)
-    {
-        float adjustedTime = 0;
-
-        switch (type)
-        {
-            case (LerpType.LINEAR):
-                {
-                    adjustedTime = lerpTime;
-                    return adjustedTime;
-                }
-
-            case (LerpType.ACCELERATED):
-                {
-                    adjustedTime = (lerpTime * lerpTime);
-                    return adjustedTime;
-                }
-
-            case (LerpType.DECELERATED):
-                {
-                    adjustedTime = (1 - (1 - lerpTime) * (1 - lerpTime));
-                    return adjustedTime;
-                }
-
-            case (LerpType.SMOOTHSTEP):
-                {
-                    float stepStart = (lerpTime * lerpTime);
-                    float stepEnd = (1 - (1 - lerpTime) * (1 - lerpTime));
-
-                    adjustedTime = Mathf.Lerp(stepStart, stepEnd, lerpTime);
-                    return adjustedTime;
-                }
-
-            default:
-                {
-                    adjustedTime = lerpTime;
-                    return adjustedTime;
-                }
-        }
-    }
-
-    private enum LerpType
-    {
-        LINEAR,
-        ACCELERATED,
-        DECELERATED,
-        SMOOTHSTEP
+        RIGHT,
+        LEFT
     };
 }
