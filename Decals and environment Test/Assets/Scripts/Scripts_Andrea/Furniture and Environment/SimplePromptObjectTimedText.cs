1fe81c4cf56de7dbb81639bd50fcfbef200b5e02
DueSette <44945710+DueSette@users.noreply.github.com>
2020-04-21 05:45:52 +0100

    Kid dialogue fix

---
+++ Decals and environment Test/Assets/Scripts/Scripts_Andrea/Furniture and Environment/SimplePromptObjectTimedText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Point of this class is to be used for anything that populates the prompt text box in the bottom center part of the UI
+ */
+
+public class SimplePromptObjectTimedText : MonoBehaviour, ITextPrompt
+{
+    [SerializeField] string promptText;
+
+    [SerializeField, Tooltip("Should the sound just linger on the screen the first time the object is hovered and then disappear?")]
+    public float lingerTime;
+
+    string ITextPrompt.PromptText()
+    {
+        Destroy(this);
+        return promptText;
+    }
+}
