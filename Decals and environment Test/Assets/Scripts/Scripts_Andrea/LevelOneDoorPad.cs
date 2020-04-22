c2456298d0cd34112147f9b46bb3e0986e5a2795
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2020-04-22 03:08:44 +0100

    Door Ver 3 Unlock And Anim

---
+++ Decals and environment Test/Assets/Scripts/Scripts_Andrea/LevelOneDoorPad.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOneDoorPad : MonoBehaviour
+{
+    [SerializeField] private Material padEmissiveMaterial;
+
+    [Header("Unlit Pad Parameters")]
+    [SerializeField] private Color unlitColor;
+    [SerializeField] private float unlitStrength;
+
+    [Header("Lit Pad Parameters")]
+    [SerializeField] private Color litColor;
+    [SerializeField] private float litStrength;
+
+    private void OnEnable()
+    {
+        LevelManager.onLevelEvent += PowerPad;
+    }
+
+    private void OnDisable()
+    {
+        LevelManager.onLevelEvent -= PowerPad;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        padEmissiveMaterial.SetColor("_EmissiveColor", unlitColor);
+        padEmissiveMaterial.SetColor("_EmissiveColorLDR", unlitColor);
+
+        padEmissiveMaterial.SetFloat("_EmissiveIntensity", unlitStrength);
+    }
+
+    void PowerPad(string eventCode)
+    {
+        if (eventCode == "FuseBoxPuzzleSolved")
+        {
+            padEmissiveMaterial.SetColor("_EmissiveColor", litColor);
+            padEmissiveMaterial.SetColor("_EmissiveColorLDR", litColor);
+
+            padEmissiveMaterial.SetFloat("_EmissiveIntensity", litStrength);
+        }
+    }
+}
